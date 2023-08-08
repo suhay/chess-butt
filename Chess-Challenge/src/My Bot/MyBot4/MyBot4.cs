9d3bc7e2a247eb9037f66358676a1518d3578252
Matt Suhay <matthew.suhay@gmail.com>
2023-08-08 15:48:10 -0500

    Optimizing transposition tables and mvv_lva table

--- Chess-Challenge/src/My Bot/MyBot4/MyBot4.cs
+++ Chess-Challenge/src/My Bot/MyBot4/MyBot4.cs
@@ -3,32 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using ChessChallenge.API;
 
-// 999
+// 924
 namespace MyBot4
 {
-  public struct Transposition
-  {
-    public Transposition(int score, int depth, int flag /*, string move*/)
-    {
-      Depth = depth;
-      Score = score;
-      Flag = flag;
-      // Move = move;
-    }
-
-    public int Depth;
-    public int Score;
-    /// <summary>
-    /// 0 = Exact, 1 = Alpha, 2 = Beta
-    /// </summary>
-    public int Flag;
-    //public string Move;
-  }
+  public record Transposition(int Score, byte Depth, byte Flag /*, string move*/);
 
   public class TranspositionTable
   {
-    public SortedList<ulong, Transposition> Table = new();
-    int maxTableSize = 20000;
+    public SortedDictionary<ulong, Transposition> Table = new();
+    int maxTableSize = 200000;
 
     public int? Get(ulong key, int depth, int alpha, int beta, int ply)
     {
@@ -61,12 +44,12 @@ namespace MyBot4
       if (score < -90000) score -= ply;
       if (score > 90000) score += ply;
 
-      Transposition newTransposition = new(score, depth, flag);
+      Transposition newTransposition = new(score, (byte)depth, (byte)flag);
       if (Table.TryGetValue(key, out var existingEntry))
       {
         Table.Remove(key);
-        // Replace on Depth if the new entry has higher depth and the existing entry is shallow (depth 0), else, keep the old one
-        Table.Add(key, depth > existingEntry.Depth && existingEntry.Depth == 0
+        // Replace on Depth if the new entry has higher depth, or always replace if it's the same
+        Table.Add(key, depth >= existingEntry.Depth
           ? newTransposition
           : existingEntry);
       }
@@ -87,62 +70,35 @@ namespace MyBot4
   public class MyBot : IChessBot
   {
     int Inf = int.MaxValue;
-    //     pub const MVV_LVA: [[u8; NrOf::PIECE_TYPES + 1]; NrOf::PIECE_TYPES + 1] = [
-    //     [10, 11, 12, 13, 14, 15, 0], // victim P, attacker K, Q, R, B, N, P, None
-    //     [20, 21, 22, 23, 24, 25, 0], // victim N, attacker K, Q, R, B, N, P, None
-    //     [30, 31, 32, 33, 34, 35, 0], // victim B, attacker K, Q, R, B, N, P, None
-    //     [40, 41, 42, 43, 44, 45, 0], // victim R, attacker K, Q, R, B, N, P, None
-    //     [50, 51, 52, 53, 54, 55, 0], // victim Q, attacker K, Q, R, B, N, P, None
-    //     [0, 0, 0, 0, 0, 0, 0],       // victim K, attacker K, Q, R, B, N, P, None
-    //     [0, 0, 0, 0, 0, 0, 0],       // victim None, attacker K, Q, R, B, N, P, None
-    // ];
-
-    /*
-        (Victims) Pawn Knight Bishop   Rook  Queen   King
-      (Attackers)
-            Pawn   105    205    305    405    505    605
-          Knight   104    204    304    404    504    604
-          Bishop   103    203    303    403    503    603
-            Rook   102    202    302    402    502    602
-           Queen   101    201    301    401    501    601
-            King   100    200    300    400    500    600
-    */
-    int[,] mvv_lva = new int[6, 6]
-    {
-      { 105, 205, 305, 405, 505, 605 },
-      { 104, 204, 304, 404, 504, 604 },
-      { 103, 203, 303, 403, 503, 603 },
-      { 102, 202, 302, 402, 502, 602 },
-      { 101, 201, 301, 401, 501, 601 },
-      { 100, 200, 300, 400, 500, 600 },
-    };
     int Depth = 3;
     Dictionary<PieceType, int> PieceVal = new()
     {
       { PieceType.None, 0 },
       { PieceType.Pawn, 100 },
-      { PieceType.Rook, 500 },
       { PieceType.Knight, 300 },
       { PieceType.Bishop, 300 },
+      { PieceType.Rook, 500 },
       { PieceType.Queen, 900 },
       { PieceType.King, 0 }
     };
     TranspositionTable TranspositionTable = new();
+    int Nodes = 0;
 
     public Move Think(Board board, Timer timer)
     {
+      Nodes = 0;
       Move[] bestMoves = NegaMaxRoot(board, Depth, -Inf, Inf, board.IsWhiteToMove ? 1 : -1);
       Random rng = new();
       Move nextMove = bestMoves[rng.Next(bestMoves.Length)];
-
+      Console.WriteLine("Nodes: {0}", Nodes);
       return nextMove;
     }
 
     Move[] NegaMaxRoot(Board board, int depth, int alpha, int beta, int color)
     {
       // Repetition history clears when a pawn moves or a capture is made. We can safely clear the transposition table
-      if (board.GameRepetitionHistory.Length == 0)
-        TranspositionTable.Table.Clear();
+      // if (board.GameRepetitionHistory.Length == 0)
+      //   TranspositionTable.Table.Clear();
 
       Move[] moves = GetOrderedMoves(board);
       if (moves.Length <= 1)
@@ -176,6 +132,7 @@ namespace MyBot4
     int MakeMove(Board board, Move move, int depth, int alpha, int beta, int color)
     {
       board.MakeMove(move);
+      Nodes++;
       if (board.IsInCheckmate())
       {
         board.UndoMove(move);
@@ -196,8 +153,9 @@ namespace MyBot4
       foreach (Move move in moves)
       {
         int score = move.IsCapture
-          ? mvv_lva[(int)move.MovePieceType - 1, (int)move.CapturePieceType - 1]
-            + (!board.SquareIsAttackedByOpponent(move.TargetSquare) ? 10700 : 10000)
+          // ? mvv_lva[(int)move.MovePieceType - 1, (int)move.CapturePieceType - 1]
+          //   + (!board.SquareIsAttackedByOpponent(move.TargetSquare) ? 10700 : 10000)
+          ? (100 * (int)move.CapturePieceType) - (int)move.MovePieceType + 6
           : 0;
         orderedMoves.Add(new MoveEvaluation(score, move));
       }
@@ -264,6 +222,7 @@ namespace MyBot4
       foreach (Move move in orderedMoves)
       {
         board.MakeMove(move);
+        Nodes++;
         int score = -Quiescence(board, -beta, -alpha, -color);
         board.UndoMove(move);
 
